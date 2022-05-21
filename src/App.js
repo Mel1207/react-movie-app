ae75820dc1b712b5173fcc5c9794317f23d055db
Mel <meljohnpualon@gmail.com>
2022-05-22 02:25:07 +0800

    added base project files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const name = "Mel"
+  const isNameShowing = false
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>hey {isNameShowing ? name: 'someone else'}</div>
     </div>
   );
 }
