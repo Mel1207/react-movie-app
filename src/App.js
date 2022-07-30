485105a9e6dec8866e73b0f44ea6583f8d49da17
Mel <meljohnpualon@gmail.com>
2022-07-30 08:57:09 +0800

    added basic get all api function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './css/main.css';
-import UseEffect from './mini-lessons/UseEffect'
+import { useEffect } from 'react';
+
+const apiURL = 'http://www.omdbapi.com?apikey=d93aeb11'
+// d93aeb11
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiURL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+    searchMovies('superman')
+  }, []);
+
   return (
     <div className="App">
       <h1>hey there</h1>
