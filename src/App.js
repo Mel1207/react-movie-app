63f2cd55c802b618060d33be08126b1d56ab3ef6
Mel <meljohnpualon@gmail.com>
2022-07-30 10:59:27 +0800

    fix card movie ui

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { useEffect } from 'react';
 const apiURL = 'http://www.omdbapi.com?apikey=d93aeb11'
 // d93aeb11
 
+const movie = {
+  "Title": "Superman Returns",
+  "Year": "2006",
+  "imdbID": "tt0348150",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BNzY2ZDQ2MTctYzlhOC00MWJhLTgxMmItMDgzNDQwMDdhOWI2XkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg"
+}
+
 const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${apiURL}&s=${title}`);
@@ -18,7 +26,18 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>hey there</h1>
+      <div className="container">
+        <h1 className='page-title'>Movie List API</h1>
+        <div className='card-movie'>
+          <img src={movie.Poster} alt={movie.Title} className='card-img'/>
+          <h3>{movie.Title}</h3>
+          <div className='card-details'>
+            <span>{movie.Year}</span>
+            <p className='movie'>{movie.Type}</p>
+          </div>
+        </div>
+      </div>
+     
     </div>
   );
 }
